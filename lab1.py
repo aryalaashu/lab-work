d409cfda0e84ff644467b80c520138b557928e66
aryalaashu <aryalaashutosh@gmail.com>
2021-12-01 12:41:20 +0545

    second commit

---
+++ lab1.py
@@ -0,0 +1,7 @@
+# sum of three numbers
+a = int(input("enter your number" ))
+b = int(input("enter your number" ))
+c = int(input("enter your number"))
+sum = a+b+c20
+30
+print(f"the sum of those numbers=", sum)
---
+++ lab2.py
@@ -0,0 +1,5 @@
+# 
+base = int(input("enter your base"))
+height = int(input("Enter your height"))
+area = 0.5*(base*height)
+print("the area of triangle", area, "cm\u00b2")
