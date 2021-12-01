c9941e5a8d6ea341720d8fa9952250ae19de4036
aryalaashu <aryalaashutosh@gmail.com>
2021-12-01 12:56:31 +0545

    volume of cuboid

---
+++ lab3.py
@@ -0,0 +1,6 @@
+# volume of cuboid
+length = int(input("Enter your length"))
+breadth = int(input("enter your breadth"))
+height = int(input("enter your height"))
+volume = length*breadth*height
+print("the volume of cuboid is", volume, "cm\u00b3")
