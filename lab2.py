bd8257b17fb470a351d0c35e72bae5d5c4b314f5
aryalaashu <aryalaashutosh@gmail.com>
2021-12-01 12:45:18 +0545

    format(f) is added

--- lab2.py
+++ lab2.py
@@ -2,4 +2,4 @@
 base = int(input("enter your base"))
 height = int(input("Enter your height"))
 area = 0.5*(base*height)
-print("the area of triangle", area, "cm\u00b2")
+print("the area of triangle", area, "cm\u00b2".format(area))
