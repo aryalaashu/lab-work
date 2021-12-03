195a41b1b47ab821e31f42ff72444bdda733a905
aryalaashu <aryalaashutosh@gmail.com>
2021-12-03 12:58:52 +0545

    day2

--- day2/lab1.py
+++ day2/lab1.py
@@ -1,4 +1,5 @@
-#convert seconds to day, hours, minutes 
+#convert seconds to day, hours, minutes and seconds
+second = 86400
 day = second/86400
 hours = second/(60*60)
 minute = second/(60)
@@ -6,3 +7,12 @@ print(day)
 print(hours)
 print(minute)
 
+
+
+second = int(input("enter second"))
+day = second/86400
+hours = second/(60*60)
+minute = second/(60)
+print(day)
+print(hours)
+print(minute)
