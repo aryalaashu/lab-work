e288226ed98e40d7da377550d9fb492f8f548787
aryalaashu <aryalaashutosh@gmail.com>
2021-12-04 20:01:39 +0545

    day2

--- day2/lab5.py
+++ day2/lab5.py
@@ -1,3 +1,13 @@
+# A school decided to replace the desks in three classrooms. Each desk sits two students.
+
+# Given the number of students in each class, print the smallest possible number of desks
+
+# that can be purchased.
+
+# The program should read three integers: the number of students in each of the three
+
+# classes, a, b and c respectively.
+
 A = int(input("enter number of students in first class"))
 B = int(input("enter number of students in second class"))
 C = int(input("enter number of students in third class"))
