fbf4b99a7bafb16c7d04b0309eca7e3200014c49
aryalaashu <aryalaashutosh@gmail.com>
2021-12-08 12:56:56 +0545

    day4

--- day4/lab2.py
+++ day4/lab2.py
@@ -4,8 +4,10 @@
 num1 = int(input("enter the number"))
 num2 = int(input("enter the number"))
 num3 = int(input("enter the number"))
+
 sum= num1+num2+num3
+
 if num1==num2 or num2==num3 or num3==num1:
-    print("you have type two number same")
+    print("0")
 else:
     print("the sum of given number =",sum )
